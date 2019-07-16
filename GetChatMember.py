25e9826ad35ab591f8f671127c1d1fa820d80f74
Hasan Vajdi <50733327+hasan-vajdi@users.noreply.github.com>
2019-07-16 23:48:40 +0430

    Create GetChatMember.py

---
+++ GetChatMember.py
@@ -0,0 +1,39 @@
+#How To Work With GetChatMember in Python_Telegram_Bot
+
+
+""" import"""
+from telegram.ext import Updater, CommandHandler, Filters
+ogging.basicConfig(level = logging.DEBUG,
+                    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+
+"""bot_token"""
+updater = Updater("YOUR_TOKEN")
+
+
+
+def GetChatMember(bot, update):
+		#paramet
+    var = bot.get_chat_member(chat_id = update.message.chat_id, user_id = update.message.from_user.id)
+    print(var)
+
+
+
+"""handlers"""
+updater.dispatcher.add_handler(CommandHandler("start", GetChatMember))
+
+
+
+updater.start_polling()
+updater.idle()
+
+
+
+
+
+result : 
+
+{'user': {'id': 343671513, 'first_name': 'SOMTHING', 'is_bot': False, 'username': 'statiic', 'language_code': 'fa'}, 'status': 'creator', 'until_date': None}
